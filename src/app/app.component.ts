7d13bad46ef8af2fa29e9de5414cb5d90f68df88
simona.loperfido <sloperfido96@gmail.com>
2021-09-10 10:34:52 +0200

    Commit tutorial

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './http.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+
+  userObject = {
+    name: 'John',
+    age: 32,
+    id: 0,
+    isColored: true
+  }
+
+  posts: any =  this.httpService.getRequest('https://jsonplaceholder.typicode.com/posts');
+  showUser: boolean= true;
+
+  constructor(private httpService: HttpService) {}
+
+  handleEvent(event: any) {
+    console.log(event);
+  }
+
+ 
+
+  ngOnInit() {
+    //this.getPosts();
+  }
+
+}
